44b569a9b76b52e11723f344135d3f43c83e529b
Enkre7 <victor.mairot@gmail.com>
2023-02-08 10:41:57 +0100

    Dynamic questions and answers

--- index.php
+++ index.php
@@ -1,16 +1,46 @@
 <?php
+// if ( ! session_id() ) {
+//     session_start();
+// }
+// if ( ! isset($_SESSION['answer_count'])) {
+//     $_SESSION['answer_count'] = 0;
+// }
+# Initialisation variables
+$answer_count = 0;
+$count = 1;
+$IsAnswered = false;
+$A_answer_count = 0;
+$B_answer_count = 0;
+$C_answer_count = 0;
+$D_answer_count = 0;
 
 # Parse le fichier JSON
 if (!(file_exists('./questions.json'))) {
     echo "Can't read file !";
 }
+
 $questions_json = file_get_contents('./questions.json');
 $decoded_json = json_decode($questions_json, true);
-
-# Compte le nombre de questions
 $questions = $decoded_json['questions'];
+
 $questions_count = count($questions);
-echo $questions_count;
+
+// $QuestionAnswered = 0;
+// if (isset($_POST['submit'])) {
+//     foreach($questions as $question) {
+//         if (isset($_POST["question_$count"])) {
+//             $QuestionAnswered++;
+//         }
+//     }
+//     echo $QuestionAnswered;
+//     if ($QuestionAnswered == count($questions)) {
+//         $IsAnswered = true;
+//     }
+// }
+
+if (isset($_POST['submit'])) {
+    $IsAnswered = true;
+}
 
 ?>
 
@@ -20,8 +50,8 @@ echo $questions_count;
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Zombie Apocalypse !</title>
-    <link rel="shortcut icon" href="./src/quiz-icon.png" type="image/x-icon">
+    <title>Zombie Apocalypse</title>
+    <link rel="shortcut icon" href="./src/zombie_favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
 </head>
@@ -33,56 +63,85 @@ echo $questions_count;
 
     <div class="container-fluid">
         <div class="jumbotron">
-            <h1>Zombie Apocalypse !</h1>
+            <h1>Zombie Apocalypse</h1>
             <h2>Quel type de survivant serez-vous ?</h2>
+            <h3>Testez votre personnalité en <?php echo $questions_count ?> questions !</h3>
         </div>
+        <?php if ($IsAnswered == true) { ?>
+                <?php
+                    foreach($_POST as $key=>$post_data){
+                        if ($post_data == 1) { $A_answer_count++; }
+                        elseif ($post_data == 2) { $B_answer_count++; }
+                        elseif ($post_data == 3) { $C_answer_count++; }
+                        elseif ($post_data == 4) { $D_answer_count++; }
+                    }
+                    if ($A_answer_count > $B_answer_count and $A_answer_count > $C_answer_count and $A_answer_count > $D_answer_count) {
+                        echo "<h2 class='answer'>Vous êtes une personne plutôt coopérative et soucieuse de l'harmonie avec les autres.</h2><br>
+                        <h2 class='answer'>Vous pouvez également faire preuve de trop de confiance et manquer de jugement.</h2>";
+                    }
+                    if ($B_answer_count > $A_answer_count and $B_answer_count > $C_answer_count and $B_answer_count > $D_answer_count) {
+                        echo "<h2 class='answer'>Vous êtes quelqu'un de déterminé et courageux.</h2><br>
+                        <h2 class='answer'>Cependant, vous pouvez également être une personne agressive et manquer de considération pour les autres.</h2>";
+                    }
+                    if ($C_answer_count > $A_answer_count and $C_answer_count > $B_answer_count and $C_answer_count > $D_answer_count) {
+                        echo "<h2 class='answer'>Vous êtes quelqu'un de rationnel et pragmatique.</h2><br>
+                        <h2 class='answer'>Cependant, vous manquez d'empathie et êtes insensible aux besoins des autres.</h2>";
+                    }
+                    if ($D_answer_count > $B_answer_count and $D_answer_count > $C_answer_count and $D_answer_count > $A_answer_count) {
+                        echo "<h2 class='answer'>Vous êtes une personne plutôt est hésitante et incertaine.</h2><br>
+                        <h2 class='answer'>Cependant, vous êtes également quelqu'un de raisonnable et soucieux de la sécurité de tous.</h2>";
+                    }
+                ?>
+        <?php } else { ?>
         <div class="row">
             <div class="col"></div>
             <div class="col">
                 <form action="" method="post">
-                    <div class="question">
-                        <div class="card" style="width:50em;">
-                            <div class="card-header">
-                                <h3 class="card-title">
-                                    <span style="font-weight: bold;">1\ Vous êtes dans un magasin d'alimentation rempli de provisions, mais vous entendez des zombies qui approchent rapidement. Que faites-vous?</span>
-                                </h3>
-                            </div>
-                            <div class="card-body">
-                                <div className='answer-section'>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="answer" id="answer1" value="1">
-                                        <label class="form-check-label" for="answer1">Vous restez et vous battez contre les zombies pour défendre les provisions.</label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="answer" id="answer2" value="2">
-                                        <label class="form-check-label" for="answer2">Vous partez en emportant autant de provisions que possible.</label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="answer" id="answer3" value="3">
-                                        <label class="form-check-label" for="answer3">Vous cachiez vous dans le magasin jusqu'à ce que les zombies soient partis.</label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="answer" id="answer4" value="4">
-                                        <label class="form-check-label" for="answer4">Vous partez sans les provisions, en vous concentrant sur votre sécurité.</label>
+                    <?php foreach($questions as $question) { ?>
+                        <div class="question">
+                            <div class="card" style="width:50em;">
+                                <div class="card-header">
+                                    <h3 class="card-title">
+                                        <span style="font-weight: bold;"><?php echo $question['question'] ?></span>
+                                    </h3>
+                                </div>
+                                <div class="card-body">
+                                    <div className='answer-section'>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="question_<?php echo $count ?>" id="radio_<?php echo $count ?>_1" value="1">
+                                            <label class="form-check-label" for="radio_<?php echo $count ?>_1"><?php echo $question['choice1'] ?></label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="question_<?php echo $count ?>" id="radio_<?php echo $count ?>_2" value="2">
+                                            <label class="form-check-label" for="radio_<?php echo $count ?>_2"><?php echo $question['choice2'] ?></label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="question_<?php echo $count ?>" id="radio_<?php echo $count ?>_3" value="3">
+                                            <label class="form-check-label" for="radio_<?php echo $count ?>_3"><?php echo $question['choice3'] ?></label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="question_<?php echo $count ?>" id="radio_<?php echo $count ?>_4" value="4">
+                                            <label class="form-check-label" for="radio_<?php echo $count ?>_4"><?php echo $question['choice4'] ?></label>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <br>
-                    <br>
-                    <br>
+                        <br>
+                        <br>
+                    <?php $count++; } ?>
                     <div class="nav-buttons">
-                        <button type="submit" class="btn btn-outline-success">Valider</button>
+                        <button type="submit" name="submit" class="btn btn-outline-success">Valider</button>
                     </div>
-                    <br>
                     <br>
                     <br>
                 </form>
             </div>
             <div class="col"></div>
         </div>
+        <?php } ?>
     </div>
+    
 </body>
 
 </html>
